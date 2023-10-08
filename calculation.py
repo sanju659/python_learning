057f4594370623ce77db2467f202d863a1c47a8e
Sanju Mondal <sanjumondal191202@gmail.com>
2023-10-08 13:20:43 +0530

    file calculation.py added

---
+++ calculation.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter n1: "))
+n2 = int(input("Enter n2: "))
+
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print("Addition is: "+str(add))
+print("Subtraction is: "+str(sub))
+print("Multiplication is: "+str(mul))
+print("Division is: "+str(div))
